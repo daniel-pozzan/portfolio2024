21b9ec27581ad0148811186ba3bac7645522311e
DESKTOP-HH0N8T9\daniel <daniel.pozzan@hotmail.com>
2024-09-22 15:25:03 +0200

    [KAN-16] Add text-skills section desktop in skills section

--- src/app/sections/skills/skills.component.ts
+++ src/app/sections/skills/skills.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TextSkillsComponent } from '../../components/text-box/text-skills/text-skills.component';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [TextSkillsComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
