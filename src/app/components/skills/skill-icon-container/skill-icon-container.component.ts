6b543b5d66f45a27086cb5e4001582c3c8f4cf6e
DESKTOP-HH0N8T9\daniel <daniel.pozzan@hotmail.com>
2024-10-07 22:21:22 +0200

    [KAN-17] Feature : Add Skill section

--- src/app/components/skills/skill-icon-container/skill-icon-container.component.ts
+++ src/app/components/skills/skill-icon-container/skill-icon-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild } from '@angular/core';
 import { SkillIconComponent } from '../skill-icon/skill-icon.component';
 import { SkillType } from '../../../shared/enums/skill-type.enums';
 
@@ -10,7 +10,47 @@ import { SkillType } from '../../../shared/enums/skill-type.enums';
   templateUrl: './skill-icon-container.component.html',
   styleUrl: './skill-icon-container.component.scss'
 })
-export class SkillIconContainerComponent {
+export class SkillIconContainerComponent implements AfterViewInit {
+  @ViewChild('swiperContainer') swiperContainer!: ElementRef;
 
   public readonly SkillType = SkillType;
+
+  ngAfterViewInit() {
+    const swiperParams = {
+      pagination: {
+        clickable: true,
+      },
+      grabCursor: true,
+      autoplay: {
+        delay: 2500,
+      },
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 3,
+          grid: {
+            rows: 2,
+          },
+          spaceBetween: 10,
+        },
+        // when window width is >= 576px
+        // 576: {
+        //   slidesPerView: 3,
+        //   grid: {
+        //     rows: 2,
+        //   },
+        // },
+        // when window width is >= 992px
+        992: {
+          slidesPerView: 3,
+          grid: {
+            rows: 3,
+          },
+          spaceBetween: 30,
+        }
+      }
+    };
+    Object.assign(this.swiperContainer.nativeElement, swiperParams);
+    this.swiperContainer.nativeElement.initialize();
+  }
 }
