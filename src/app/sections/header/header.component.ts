e8c7dd690b960243fda365cc00ae97220d909b3e
DESKTOP-HH0N8T9\daniel <daniel.pozzan@hotmail.com>
2024-12-30 18:04:29 +0100

    Add logo and favicon

--- src/app/sections/header/header.component.ts
+++ src/app/sections/header/header.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MenuComponent } from '../../components/menu/menu.component';
 import { ViewportType } from '../../shared/enums/viewport-type.enums';
 import { BurgerCloseButtonComponent } from '../../components/buttons/burger-close-button/burger-close-button.component';
+import { LogoComponent } from '../../components/logo/logo.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MenuComponent, BurgerCloseButtonComponent],
+  imports: [CommonModule, MenuComponent, BurgerCloseButtonComponent, LogoComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
